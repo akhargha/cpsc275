c651ebc852ce307e6a716d60ba471a7b6df2f67d
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-10-24 23:33:28 -0400

    Comments

--- assn2/assem.c
+++ assn2/assem.c
@@ -1,6 +1,17 @@
+/**
+ * assem.c : decode binary string representation of instructions
+ * CPSC 275 Assignment 2
+ * @author Anupam Khargharia
+*/
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Convert a binary string representation into numeric (later converted to hex).
+ * 
+ * @param char *binary: A string consisting of 0s and 1s.
+ * @return unsigned short num: The number represented by the binary string.
+ */
 unsigned short binstr2num(char *binary) {
     unsigned short num = 0;
     for (int i = 0; i < 16; i++) {
@@ -11,11 +22,19 @@ unsigned short binstr2num(char *binary) {
     return num;
 }
 
+/**
+ * Decode an instruction and display its opcode, mnemonic, and operand.
+ *
+ * @param unsigned short instr: the instruction returned by binstr2num()
+ * @param unsigned short opcode: the location of the opcode
+ * @param char mnem: the location of the mnemonic
+ * @param unsigned short opn: the location of the operand
+ * @return int m: the type of operand
+ */
 int decode(unsigned short instr, unsigned short *opcode, char *mnem, unsigned short *opn) {
-    
-    *opcode = (instr >> 12) & 0xF; // first 4 bits
-    int m = (instr >> 11) & 0x1; //5th bit
-    *opn = instr & 0x7FF; //last 11 bits
+    *opcode = (instr >> 12) & 0xF; // 0 to 3
+    int m = (instr >> 11) & 0x1; // 4
+    *opn = instr & 0x7FF; // 5 to 15
 
     if (*opcode == 0x0) {
         strcpy(mnem, "EOC   ");
